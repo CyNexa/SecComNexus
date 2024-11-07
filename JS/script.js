cf20c369cd8be839d1d5d34e8a293735df27f60a
Titas Mandal <stepnexx@gmail.com>
2024-11-08 00:38:03 +0530

    About-Comp

--- JS/script.js
+++ JS/script.js
@@ -12,3 +12,26 @@ window.addEventListener('scroll', () => {
     
     lastScrollTop = scrollTop;
 });
+
+const logo = document.getElementById('logo');
+const darkModeToggle = document.getElementById('dark-mode-toggle');
+
+if (localStorage.getItem('theme') === 'dark') {
+  document.documentElement.classList.add('dark-mode');
+  logo.src = 'Assets/seccom_logo-b.webp';
+  darkModeToggle.textContent = "☀️";
+}
+
+darkModeToggle.addEventListener('click', () => {
+  document.documentElement.classList.toggle('dark-mode');
+
+  if (document.documentElement.classList.contains('dark-mode')) {
+    localStorage.setItem('theme', 'dark');
+    logo.src = 'Assets/seccom_logo-w.webp'; // Path for the dark version of the logo
+    darkModeToggle.textContent = "☀️";
+  } else {
+    localStorage.setItem('theme', 'light');
+    logo.src = 'Assets/seccom_logo-b.webp'; // Path for the light version of the logo
+    darkModeToggle.textContent = "🌙";
+  }
+});
